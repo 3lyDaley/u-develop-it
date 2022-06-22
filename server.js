1363831e2766cd5c592159818b8e13ea2034ecaa
Bad Boys Club <3ly.daley@gmail.com>
2022-06-21 23:46:08 -0700

    added db query

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql =require('mysql2');
 const express = require('express');
 const PORT= process.env.PORT || 3001;
 const app = express();
@@ -6,18 +7,27 @@ const app = express();
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// connect to database
 
-app.get('/', (req,res) => {
-  res.json({
-    message: 'Hello World'
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: '?2rKeKTJR?',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+  );
+  
+  db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
   });
-});
-
 // Default response for any other request (Not Found)
 app.use((req, res) => {
- res.status(404).end();
+  res.status(404).end();
+});
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
