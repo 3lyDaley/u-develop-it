50bb213e85877df1add39a291db59b463fe2d248
Bad Boys Club <3ly.daley@gmail.com>
2022-06-22 18:07:32 -0700

    populated parties table

--- db/schema.sql
+++ db/schema.sql
@@ -1,21 +1,18 @@
 DROP DATABASE IF EXISTS election;
 
-CREATE TABLE parties
-(
-  id INTEGER
-  AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR
-  (50) NOT NULL,
+CREATE DATABASE election;
+
+use election;
+
+CREATE TABLE parties (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
   description TEXT
 );
 
-  CREATE TABLE candidates
-  (
-    id INTEGER
-    AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR
-    (30) NOT NULL,
-  last_name VARCHAR
-    (30) NOT NULL,
+CREATE TABLE candidates (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR (30) NOT NULL,
+  last_name VARCHAR (30) NOT NULL,
   industry_connected BOOLEAN NOT NULL
 );
